4f7eeadc3281add54368c82bba7b052d3f6cae0b
Fernando <fernando.teixeira6@outlook.com>
2026-01-06 00:44:09 -0300

    Run EF Core migrations on startup

--- Program.cs
+++ Program.cs
@@ -72,8 +72,13 @@ app.MapTimeEntryStatus();
 
 app.UseHttpsRedirection();
 
-
-
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 
 app.Run();
